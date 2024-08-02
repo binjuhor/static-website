de8a8cbc078932e50494b29d88688568351b81e4
Hoang Kiem <kiemhd@outlook.com>
2024-08-02 19:56:53 +0700

    feat: update new structure

--- helpers.php
+++ helpers.php
@@ -51,3 +51,32 @@ function activate($page)
 
     return '';
 }
+
+function initContent($dispatcher)
+{
+    $httpMethod = $_SERVER['REQUEST_METHOD'];
+    $uri = $_SERVER['REQUEST_URI'];
+
+    if (false !== $pos = strpos($uri, '?')) {
+        $uri = substr($uri, 0, $pos);
+    }
+    $uri = rawurldecode($uri);
+
+    $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+    switch ($routeInfo[0]) {
+        case FastRoute\Dispatcher::NOT_FOUND:
+            http_response_code(404);
+            echo '404 Not Found';
+            break;
+        case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+            http_response_code(405);
+            echo '405 Method Not Allowed';
+            break;
+        case FastRoute\Dispatcher::FOUND:
+            $handler = $routeInfo[1];
+            $vars = $routeInfo[2];
+            $handler($vars);
+            break;
+    }
+}
--- index.php
+++ index.php
@@ -1,31 +1,9 @@
 <?php
 require_once "vendor/autoload.php";
 
-$dispatcher = routeList();
-
-$httpMethod = $_SERVER['REQUEST_METHOD'];
-$uri = $_SERVER['REQUEST_URI'];
-
-if (false !== $pos = strpos($uri, '?')) {
-    $uri = substr($uri, 0, $pos);
-}
-$uri = rawurldecode($uri);
-
-$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
-
-switch ($routeInfo[0]) {
-    case FastRoute\Dispatcher::NOT_FOUND:
-        page('404');
-        break;
-    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-        $allowedMethods = $routeInfo[1];
-        page('405');
-        break;
-    case FastRoute\Dispatcher::FOUND:
-        $handler = $routeInfo[1];
-        $vars = $routeInfo[2];
-        page($handler, $vars);
-        break;
-}
-
-
+initContent(FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', 'home');
+    $r->addRoute('GET', '/users', 'users');
+    $r->addRoute('GET', '/user/{id:\d+}', 'test');
+    $r->addRoute('GET', '/widget/{id:\d+}[/{name}]', 'sport');
+}));
